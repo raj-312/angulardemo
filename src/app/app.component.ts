532c463a0e07ef27d80cb5a3554e42f9a185f7bf
Rajveer Singh <36976099+raj-312@users.noreply.github.com>
2023-05-10 09:01:26 +1000

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,83 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { ColDef, RowGroupingDisplayType } from 'ag-grid-community';
+import '../styles.css';
+// Required feature modules are registered in app.module.ts
+@Component({
+  selector: 'app-root',
+  template: `<ag-grid-angular
+    style="width: 100%;height: 94vh;padding: 50px;"
+    class="ag-theme-alpine"
+    [columnDefs]="columnDefs"
+    [defaultColDef]="defaultColDef"
+    [groupDisplayType]="groupDisplayType"
+    [groupRowRendererParams]="groupRowRendererParams"
+    [groupDefaultExpanded]="groupDefaultExpanded"
+    [groupRemoveSingleChildren]="true"
+    [animateRows]="true"
+    [rowData]="rowData"
+    (gridReady)="onGridReady()"
+  ></ag-grid-angular>`,
+})
+export class AppComponent {
+  public columnDefs: ColDef[] = [
+    {
+      headerName: '',
+      width: 45,
+      checkboxSelection: true,
+      headerCheckboxSelection: true,
+      suppressMenu: true,
+      resizable: true,
+      pinned: 'left',
+      lockPinned: true,
+      lockPosition: true,
+      field: '',
+      suppressColumnsToolPanel: true,
+      editable: false,
+      suppressSizeToFit: true,
+      sortable: true,
+      suppressMovable: true,
+      suppressFiltersToolPanel: true,
+      suppressNavigable: true,
+      suppressCellFlash: true
+    },
+    { field: 'country', rowGroup: true,filter: 'agTextColumnFilter' },
+    { field: 'year', sortable: true,filter: 'agTextColumnFilter' },
+    { field: 'athlete', minWidth: 250,filter: 'agTextColumnFilter' },
+    { field: 'sport', minWidth: 200,filter: 'agTextColumnFilter' },
+    { field: 'gold' },
+    { field: 'silver' },
+    { field: 'bronze' },
+  ];
+  public defaultColDef: ColDef = {
+    flex: 1,
+    minWidth: 100,
+    sortable: true,
+    resizable: true,
+  };
+  public groupDisplayType: RowGroupingDisplayType = 'groupRows';
+  public rowGroupPanelShow: 'always' | 'onlyWhenGrouping' | 'never' = 'always';
+
+  public groupRowRendererParams = {
+    // puts a checkbox onto each group row
+    checkbox: false,
+    // puts a row dragger onto each group row
+    rowDrag: false
+  };
+
+  public groupDefaultExpanded = 0;
+  public rowData!: [];
+
+  constructor(private http: HttpClient) { }
+
+  onGridReady() {
+    this.http
+      .get<any>(
+        'https://www.ag-grid.com/example-assets/olympic-winners.json'
+      )
+      .subscribe((data) => {
+
+        this.rowData = data;
+      });
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { LicenseManager } from 'ag-grid-enterprise';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+LicenseManager.setLicenseKey('[TRIAL]_CompanyName=SYSTEM,LicensedApplication=Trial Support - April,LicenseType=SingleApplication,LicensedConcurrentDeveloperCount=-1,LicensedProductionInstancesCount=0,AssetReference=AG-037732,SupportServicesEnd=30_April_2023_[v2]_MTY4MjgwOTIwMDAwMA==ac53951bd0f776b7f44f66ab27d46014');
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
